88f52eb7a046c1c62da79907b34c79124e44406f
Neel Sani <neel@neels.dev>
2025-07-04 05:02:31 +0000

    pevent error

--- src/dialects/dialects.zig
+++ src/dialects/dialects.zig
@@ -1,36 +1,6 @@
-pub const storm32 = struct {
-    pub const enums    = @import("storm32/enums.zig");
-    pub const messages = @import("storm32/messages.zig");
-};
-
-
-pub const minimal = struct {
-    pub const enums    = @import("minimal/enums.zig");
-    pub const messages = @import("minimal/messages.zig");
-};
-
-
-pub const development = struct {
-    pub const enums    = @import("development/enums.zig");
-    pub const messages = @import("development/messages.zig");
-};
-
-
-pub const standard = struct {
-    pub const enums    = @import("standard/enums.zig");
-    pub const messages = @import("standard/messages.zig");
-};
-
-
 pub const icarous = struct {
     pub const enums    = @import("icarous/enums.zig");
     pub const messages = @import("icarous/messages.zig");
-};
-
-
-pub const loweheiser = struct {
-    pub const enums    = @import("loweheiser/enums.zig");
-    pub const messages = @import("loweheiser/messages.zig");
 };
 
 
@@ -40,9 +10,51 @@ pub const matrixpilot = struct {
 };
 
 
+pub const ualberta = struct {
+    pub const enums    = @import("ualberta/enums.zig");
+    pub const messages = @import("ualberta/messages.zig");
+};
+
+
 pub const paparazzi = struct {
     pub const enums    = @import("paparazzi/enums.zig");
     pub const messages = @import("paparazzi/messages.zig");
+};
+
+
+pub const ardupilotmega = struct {
+    pub const enums    = @import("ardupilotmega/enums.zig");
+    pub const messages = @import("ardupilotmega/messages.zig");
+};
+
+
+pub const standard = struct {
+    pub const enums    = @import("standard/enums.zig");
+    pub const messages = @import("standard/messages.zig");
+};
+
+
+pub const uAvionix = struct {
+    pub const enums    = @import("uAvionix/enums.zig");
+    pub const messages = @import("uAvionix/messages.zig");
+};
+
+
+pub const loweheiser = struct {
+    pub const enums    = @import("loweheiser/enums.zig");
+    pub const messages = @import("loweheiser/messages.zig");
+};
+
+
+pub const csAirLink = struct {
+    pub const enums    = @import("csAirLink/enums.zig");
+    pub const messages = @import("csAirLink/messages.zig");
+};
+
+
+pub const storm32 = struct {
+    pub const enums    = @import("storm32/enums.zig");
+    pub const messages = @import("storm32/messages.zig");
 };
 
 
@@ -64,21 +76,9 @@ pub const Test = struct {
 };
 
 
-pub const ualberta = struct {
-    pub const enums    = @import("ualberta/enums.zig");
-    pub const messages = @import("ualberta/messages.zig");
-};
-
-
 pub const AVSSUAS = struct {
     pub const enums    = @import("AVSSUAS/enums.zig");
     pub const messages = @import("AVSSUAS/messages.zig");
-};
-
-
-pub const all = struct {
-    pub const enums    = @import("all/enums.zig");
-    pub const messages = @import("all/messages.zig");
 };
 
 
@@ -88,27 +88,27 @@ pub const ASLUAV = struct {
 };
 
 
+pub const all = struct {
+    pub const enums    = @import("all/enums.zig");
+    pub const messages = @import("all/messages.zig");
+};
+
+
+pub const development = struct {
+    pub const enums    = @import("development/enums.zig");
+    pub const messages = @import("development/messages.zig");
+};
+
+
+pub const minimal = struct {
+    pub const enums    = @import("minimal/enums.zig");
+    pub const messages = @import("minimal/messages.zig");
+};
+
+
 pub const common = struct {
     pub const enums    = @import("common/enums.zig");
     pub const messages = @import("common/messages.zig");
-};
-
-
-pub const ardupilotmega = struct {
-    pub const enums    = @import("ardupilotmega/enums.zig");
-    pub const messages = @import("ardupilotmega/messages.zig");
-};
-
-
-pub const csAirLink = struct {
-    pub const enums    = @import("csAirLink/enums.zig");
-    pub const messages = @import("csAirLink/messages.zig");
-};
-
-
-pub const uAvionix = struct {
-    pub const enums    = @import("uAvionix/enums.zig");
-    pub const messages = @import("uAvionix/messages.zig");
 };
 
 
